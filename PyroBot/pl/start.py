0a232b065430dbe841fb53fea8dd827aa5fd31ae
TimmyGahmen <144558199+timgahmen@users.noreply.github.com>
2025-04-09 07:57:26 +0200

    Update start.py

--- PyroBot/pl/start.py
+++ PyroBot/pl/start.py
@@ -1,20 +1,22 @@
+# https://github.com/GahmenGang/
+
 from pyrogram import filters
 from pyrogram.types import (
     InlineKeyboardButton,
     InlineKeyboardMarkup
 )
 
-from PyroBot import excmd
+from PyroBot import Bot
 
 MESSAGE = (
         'Hello there!\n'
         'I am a Simple Bot Build with Pyrogram and Python\n\n'
-        'By [exploiterxD](https://github.com/exploiterxD)'
+        'By [GahmenGang](https://github.com/GahmenGang/)'
     )
 
 KEYBOARD = InlineKeyboardMarkup(
-    [[InlineKeyboardButton(text='Github', url='github.com/exploiterxD')],
-    [InlineKeyboardButton(text='Source Code', url='github.com/exploiterxD/BasicPyrogramBot')]]
+    [[InlineKeyboardButton(text='Github', url='github.com/GahmenGang')],
+    [InlineKeyboardButton(text='Source Code', url='github.com/GahmenGang/BasicPyrogramBot_Template')]]
 )
 
 @excmd.on_message(filters.text & filters.private & ~filters.bot)
