fee81b76425104629a682daa92ec024236637c0f
TimmyGahmen <144558199+timgahmen@users.noreply.github.com>
2025-04-09 08:00:05 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# https://github.com/GahmenGang/
+
+# Note if you are trying to deploy on vps then directly fill values in ("")
+
+# get from [Telegram.org](https://my.telegram.org/auth)
+API_ID = ""
+API_HASH = ""
+
+# get from @Botfather on telegram
+BOT_TOKEN = ""
